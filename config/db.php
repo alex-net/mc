194c6a221a8879c8334fdf778d6246b3e2df41ad
Alex Net <alexnet2004@gmail.com>
2018-11-10 14:30:12 +0300

    вернули подключение на mysql

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=mc',
+    'dsn' => 'mysql:host=localhost;dbname=mc',
     'username' => 'mc',
     'password' => 'mc',
     'charset' => 'utf8',
